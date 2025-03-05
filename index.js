41aea40ea442ba6f18d2c02d054f4095c241f13e
Ravishankar Vishwakarma <84084665+rjrvsharma@users.noreply.github.com>
2025-03-05 14:12:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 4000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node!\n'
+  const msg = 'Hello Abbu!\n'
   res.end(msg);
 });
 
